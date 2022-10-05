e251049f05c0bfb901418887eb0a76fc1393c571
Andrey Milykh <amilykh@mail.ru>
2022-10-05 22:31:15 +0300

    Milykh_Andrey_dz_4

---
+++ Milykh_Andrey_dz_4/Milykh_Andrey_dz_4.sql
@@ -0,0 +1,88 @@
+DROP DATABASE IF EXISTS milykh_andrey_dz_4;
+CREATE DATABASE milykh_andrey_dz_4;
+USE milykh_andrey_dz_4;
+
+CREATE TABLE  AUTO 
+(       
+	REGNUM VARCHAR(10) PRIMARY KEY, 
+	MARK VARCHAR(10), 
+	COLOR VARCHAR(15),
+	RELEASEDT DATE, 
+	PHONENUM VARCHAR(15)
+); 
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111114,'LADA', 'КРАСНЫЙ', date'2008-01-01', '9152222221');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111115,'VOLVO', 'КРАСНЫЙ', date'2013-01-01', '9173333334');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111116,'BMW', 'СИНИЙ', date'2015-01-01', '9173333334');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111121,'AUDI', 'СИНИЙ', date'2009-01-01', '9173333332');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111122,'AUDI', 'СИНИЙ', date'2011-01-01', '9213333336');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111113,'BMW', 'ЗЕЛЕНЫЙ', date'2007-01-01', '9214444444');
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111126,'LADA', 'ЗЕЛЕНЫЙ', date'2005-01-01', null);
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111117,'BMW', 'СИНИЙ', date'2005-01-01', null);
+INSERT INTO AUTO (REGNUM, MARK,	COLOR, RELEASEDT, PHONENUM )
+VALUES(111119,'LADA', 'СИНИЙ', date'2017-01-01', 9213333331);
+
+DROP TABLE IF EXISTS test_a;
+CREATE TABLE test_a(
+	id INT, 
+    `data` VARCHAR(1));    
+;
+INSERT INTO test_a(id, `data`)
+VALUES
+	(10, 'A'),
+    (20, 'A'),
+    (30, 'F'),
+    (40, 'D'),
+    (50, 'C');
+
+DROP TABLE IF EXISTS test_b;
+CREATE TABLE test_b(id INT);
+INSERT INTO test_b(id) 
+VALUES
+	(10), (30), (50);
+
+
+/*
+Задача 1
+
+Вывести на экран, сколько машин каждого цвета для машин
+макрок BMW и LADA
+*/
+SELECT MARK, COLOR, COUNT(*) AS QUANTITY
+FROM AUTO
+WHERE MARK IN ('BMW', 'LADA')
+GROUP BY MARK, COLOR
+ORDER BY MARK, COLOR;
+
+/*
+Задача 2
+
+Вывести на экран марку авто(количество) и количество 
+не этой марки
+*/
+SELECT MARK, 
+	   COUNT(*) AS QUANTITY,
+       (SELECT COUNT(*) FROM AUTO) - COUNT(*) AS OTHERS
+FROM AUTO
+GROUP BY MARK
+ORDER BY MARK;
+
+/*
+Задача 3
+
+Напишите запрос, который вернёт строки из таблицы test_a,
+id которых нет в таблице test_b, НЕ используя ключевого
+слова NOT.
+*/
+SELECT a.id, a.`data`
+FROM test_a a
+LEFT JOIN test_b b
+ON b.id = a.id
+WHERE b.id IS NULL;
